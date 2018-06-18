725af572fccf01bc6f874f011e0bed6f84421832
James Townsend <jamestownsend@Jamess-MacBook-Air.local>
2018-06-17 21:44:31 -0600

    yes

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+// we need four crystals with random numbers
+
+//everytime we win or lose you generate crystal 1-12
+
+//when you click a crystal, it should be adding previous resault till it hits total score.
+
+// if the number is not equal start over. decrement the lose counter. 
+
+// if it is equal increment the win counter.
+
+
+
+
+
